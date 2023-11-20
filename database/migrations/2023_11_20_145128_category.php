a29cf536c406062b7ed3bcdb7da557903521c65c
Tran Ngoc Tien <tranngoctien@Trans-MacBook-Air.local>
2023-11-20 21:57:43 +0700

    update data

--- database/migrations/2023_11_20_145128_category.php
+++ database/migrations/2023_11_20_145128_category.php
@@ -13,11 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('Categorie', function (Blueprint $table) {
+        Schema::create('Category', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
         });
+
     }
 
     /**
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Categorie');
+        //
     }
 };
